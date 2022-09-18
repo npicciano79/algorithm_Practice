889566774739fa60f838d6027a10bd4b450266e9
npicciano79 <npicciano79@gmail.com>
2022-09-18 08:16:14 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -43,13 +43,62 @@ solution=Solution()
 print(solution.fibbonacci(value))
 """
 
-
+"""
 class Solution(object):
     def fib(self,n):
         if n<=2:
             return n
         else:
-            return self.fib(n-2)+self.fib(n-1)
+            return self.fib(n-1)+self.fib(n-2)
+            
+            
+
+
+memo=[]
+solution=Solution()
+print(solution.fib(9))
+"""
+
+"""
+#memoize solution
+class Solution(object):
+    def fib(self,n,memo):
+        if n<len(memo):
+            return memo[n]
+        else:
+            memo.append(self.fib(n-1,memo)+self.fib(n-2,memo))
+            return memo[n]
+            
+            
+
+
+memo=[0,1]
+solution=Solution()
+print(solution.fib(9,memo))
+"""
+
+
+#solution printing all fib values in sequence
+class Solution(object):
+    def fibItterative(self,n):
+        count=0
+        seq=[]
+        n1,n2=0,1
+        while count<=n:
+            num=n1
+            seq.append(num)
+            n1=n2
+            n2+=num
+            count+=1
+        
+        return seq
+
+
+
+
+
+
+
 
 solution=Solution()
-print(solution.fib(5))
+print(solution.fibItterative(10))
